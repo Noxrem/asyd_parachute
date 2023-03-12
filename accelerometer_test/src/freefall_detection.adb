d15872cc7e4c3a2137032f50f3cf9585f061e5f4
Noxrem <skular.trix@hotmail.ch>
2023-03-12 23:43:12 +0100

    Changed from float to only integer use
    Debugging does not work anymore

---
+++ accelerometer_test/src/freefall_detection.adb
@@ -0,0 +1,17 @@
+package body freefall_detection is
+
+   --  --  function get_S_Factor (data : LSM303.All_Axes_Data) return Float is
+   --  --  begin
+   --  --  return Sqrt(Float(data.X) ** 2 + Float(data.Y) ** 2 + Float(data.Z) ** 2);
+   --  -- end get_S_Factor;
+
+   function get_S_Factor_Sqrd (data : LSM303.All_Axes_Data) return S_Factor_Sqrd_t is
+      S_factor : Integer;
+      S_factor_type : S_Factor_Sqrd_t;
+   begin
+      S_factor := Integer(data.X ** 2 + data.Y ** 2 + data.Z ** 2);
+      S_factor_type := S_Factor_Sqrd_t(S_factor);
+      return S_factor_type;
+   end get_S_Factor_Sqrd;
+
+end freefall_detection;
--- accelerometer_test/src/main.adb
+++ accelerometer_test/src/main.adb
@@ -7,29 +7,21 @@ with LSM303; use LSM303;
 with MicroBit.Display;
 with MicroBit.Accelerometer;
 with MicroBit.Console;
-with MicroBit.IOs;
+--with MicroBit.IOs;
 with MicroBit.Time;
 
-with Cortex_M.FPU;
-with Ada.Text_IO;
+with freefall_detection;
 
 use MicroBit;
-use MicroBit.IOs;
-use Cortex_M.FPU;
+--use MicroBit.IOs;
+use freefall_detection;
 
 
 procedure Main is
 
    Data : LSM303.All_Axes_Data;
-   Value : MicroBit.IOs.Analog_Value;
-   sFactor : Float;
-   str_sFactor : String(1..10);
-
-
-   function get_S_Factor (data : LSM303.All_Axes_Data) return Float is
-   begin
-      return Sqrt(Float(data.X) ** 2 + Float(data.Y) ** 2 + Float(data.Z) ** 2);
-   end get_S_Factor;
+   --Value : MicroBit.IOs.Analog_Value;
+   sFactor_Sqrd : S_Factor_Sqrd_t;
 
 begin
 
@@ -44,35 +36,30 @@ begin
       Console.Put_Line ("X:" & Data.X'Img & ASCII.HT &
                         "Y:" & Data.Y'Img & ASCII.HT &
                           "Z:" & Data.Z'Img);
-      -- Print the S-Factor of the accelerator data (convert Float to Integer
-      -- then into a printable string)
-      --sFactor := get_S_Factor(Data);
-      sFactor := 3.4;
-      str_sFactor := Float'Image(sFactor);
-      Console.Put_Line ("S-Factor:" & str_sFactor);
-      --Console.Put_Line ("S-Factor:" & Float'Image(sFactor));
-      --Console.Put (Float'Image(sFactor));
+      -- Print the S-Factor of the accelerator data (convert Float to String)
+      sFactor_Sqrd := get_S_Factor_Sqrd(Data);
+      Console.Put_Line ("S-Factor squared:" & sFactor_Sqrd'Img);
 
       --  Clear the LED matrix
       Display.Clear;
 
-      -- Check, whether we are free floating or not (to be refined ...)
-      if -100 < Data.X and Data.X < 100 then
-         Display.Display ('0');
-      else
-         Display.Display ('X');
-      end if;
-
-      -- Read analogue pin (could be 0,1,2,3,4, or 10)
-      Value := MicroBit.IOs.Analog (2);
-      Console.Put_Line ("Value : " & Value'Image);
-
-      -- Set output
-      if Value > Analog_Value(200) then
-         MicroBit.IOs.Set (12, True);
-      else
-         MicroBit.IOs.Set (12, False);
-      end if;
+      --  -- Check, whether we are free floating or not (to be refined ...)
+      --  if -100 < Data.X and Data.X < 100 then
+      --     Display.Display ('0');
+      --  else
+      --     Display.Display ('X');
+      --  end if;
+      --
+      --  -- Read analogue pin (could be 0,1,2,3,4, or 10)
+      --  Value := MicroBit.IOs.Analog (2);
+      --  Console.Put_Line ("Value : " & Value'Image);
+      --
+      --  -- Set output
+      --  if Value > Analog_Value(200) then
+      --     MicroBit.IOs.Set (12, True);
+      --  else
+      --     MicroBit.IOs.Set (12, False);
+      --  end if;
 
       Time.Sleep (50);
    end loop;
