e5dec9665b5eb6004685770807992bd8526ce002
Noxrem <skular.trix@hotmail.ch>
2023-03-20 01:16:36 +0100

    Implemented 'X' and 'O' at freefall detection
    Tried to implement the music library but it caused a fault

--- accelerometer_test/src/main.adb
+++ accelerometer_test/src/main.adb
@@ -7,14 +7,17 @@ with LSM303; use LSM303;
 with MicroBit.Display;
 with MicroBit.Accelerometer;
 with MicroBit.Console;
---with MicroBit.IOs;
+with MicroBit.IOs;
 with MicroBit.Time;
+with MicroBit.Music;
 
 with freefall_detection;
 
 use MicroBit;
 --use MicroBit.IOs;
 use freefall_detection;
+use MicroBit.Music;
+use MicroBit.IOs;
 
 
 procedure Main is
@@ -24,6 +27,12 @@ procedure Main is
    sFactor_Sqrd : S_Factor_Sqrd_t;
    iFallCounter : Integer := 0;
    iFallCounterThresh : constant Integer := 3;
+   bFreefallDetected : Boolean := False;   -- Becomes true when a freefall
+                                           -- got detected
+   tonePin : constant Pin_Id := 0;                  -- Pin for the speaker
+   toneDuration : constant Time.Time_Ms := 200;    -- Duration of a tone
+   nFreefallTone : constant Note := (P => A4,              -- Tone played on freefall
+                            Ms => toneDuration);
 
 begin
 
@@ -34,6 +43,8 @@ begin
       --  Read the accelerometer data
       Data := Accelerometer.Data;
 
+      bFreefallDetected := False;   -- Initialize the freefall detection
+
       --  Print the data on the serial port
       --  Console.Put_Line ("X:" & Data.X'Img & ASCII.HT &
       --                    "Y:" & Data.Y'Img & ASCII.HT &
@@ -43,6 +54,7 @@ begin
       if sFactor_Sqrd <= 3000 then -- before 6000
          if iFallCounter >= iFallCounterThresh then
             Console.Put_Line ("S-Factor squared:" & sFactor_Sqrd'Img);
+            bFreefallDetected := True;
             iFallCounter := 0; -- Reset
          else
             iFallCounter := iFallCounter + 1; -- Increment counter
@@ -51,6 +63,13 @@ begin
       --  Clear the LED matrix
       Display.Clear;
 
+      -- Displays a 'F' if freefall is detected and a 'O' if not
+      if bFreefallDetected then
+         Display.Display('F');
+         Play(Pin => tonePin, N => nFreefallTone); -- Play tone when in freefall
+      else
+         Display.Display('O');
+      end if;
       --  -- Check, whether we are free floating or not (to be refined ...)
       --  if -100 < Data.X and Data.X < 100 then
       --     Display.Display ('0');
